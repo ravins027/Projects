d38bf43d875127cef30e9afcedc84304d1f1531a
ravins027 <66437673+ravins027@users.noreply.github.com>
2021-04-09 00:35:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+<script>
+
+function checkforblank(form)
+{
+ if (form.inputfield.value == ""){
+    alert("errormessage");
+    form.inputfield.focus();
+    return false;
+ }
+
+
+}
+
+</script>
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+ 
+
+
+
+
+/*
+/*$(document).ready(function() { 
+            $("inputs").focusout(function() { 
+                if($(this).val()=='') { 
+                    $(this).css('border', 'solid 2px red'); 
+                }
+                else {
+                      
+                    // If it is not blank.
+                    $(this).css('border', 'solid 2px green');    
+                }    
+            }) .trigger("focusout");
+        }); 
+*/
+/*
+
+<script>
+function checkforblank() {
+
+    if (document.getElementById('cname').value == "") {
+        alert('Please enter your first name');
+        document.getElementById('cname').style.borderColor = "red";
+        return false;
+        }
+
+    }    
+        
+        var input = document.getElementById('form').style.borderColor = 'red';
+    }
+    if (document.getElementById('form').value == "") {
+        errormessage += "skriv dit fornavn \n";
+        document.getElementById('form').style.borderColor = "red";
+}
+
+    if (errormessage != "") {
+        alert(errormessage);
+        return false;
+        }
+    }
+
+    </script>*/*/*/
+
+
+
+
+
+
+
+
+
+
